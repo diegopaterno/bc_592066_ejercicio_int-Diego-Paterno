9e838007024de2f0ce3ce8fb21af524e3c6923ac
Diego Martin Paterno <diego.paterno@globalprocessing.com.ar>
2022-11-16 13:15:52 -0300

    agrego archivos

---
+++ js/servicios/productos.js
@@ -0,0 +1,65 @@
+/* class ProductoService {
+    URL_PRODUCTOS = 'https://615d8b5212571a00172076ba.mockapi.io/productos/'
+
+    async obtenerProductosService() {
+        let productos = await http.get(this.URL_PRODUCTOS)
+        return productos
+    }
+
+    async guardarProductoService(producto) {
+        const productoGuardado = await http.post(this.URL_PRODUCTOS, producto)
+        // console.log(productoGuardado)
+        return productoGuardado
+    }
+
+    async actualizarProductoService(id, producto) {
+        const productoActualizado = await http.put(this.URL_PRODUCTOS, id, producto)
+        // console.log(productoActualizado)
+        return productoActualizado
+    }
+
+    async borrarProductoService(id) {
+        const productoBorrado = await http.del(this.URL_PRODUCTOS, id)
+        // console.log(productoBorrado)
+        return productoBorrado
+    }
+    
+}
+
+const productoService = new ProductoService()
+ */
+
+class ProductoService {
+    URL_PRODUCTOS = 'https://633ccbddf2b0e623dc67cdb8.mockapi.io/productos/';
+
+    async obtenerProductosService() {
+        let productos = await http.get(this.URL_PRODUCTOS);
+        console.log(productos);
+        return productos
+    }
+    
+    async guardarProductosService(producto) {
+        const productoGuardado = await http.post(this.URL_PRODUCTOS, producto);
+        //console.log(productoGuardado)
+        return productoGuardado
+    }
+    
+    
+    async actualizarProductosService(id, producto) {
+        const productoActualizado = await http.put(this.URL_PRODUCTOS, id, producto);
+        //console.log(productoActualizado)
+        return productoActualizado
+    
+    }
+    
+    
+    async borrarProductosService(id) {
+        const productoBorrado = await http.del(this.URL_PRODUCTOS, id);
+            //console.log(productoBorrado)
+            return productoBorrado
+    
+    }
+    
+}
+
+const productoService = new ProductoService()
